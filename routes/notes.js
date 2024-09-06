7957ad989aa55f0d26a30ad036ef374f1e001049
gustavovargas511 <gustavovargas511@gmail.com>
2024-09-06 01:10:49 -0600

    Changes to be committed: 	new file:   config/db.js 	modified:   routes/notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,27 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mysqlConn = require("mysql2");
-require("dotenv").config();
-
-//** Connection */
-
-const connection = mysqlConn.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.log("Error connectiong to notes db", err);
-    return;
-  }
-
-  console.log("Connection to notes api successful");
-});
-/************** */
+const connection = require("../config/db");
 
 /** Get ALL Notes */
 router.get("/", (req, res) => {
